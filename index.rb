69000eedf90d2edbc8693cebbf2419c6d2633c34
Frankie Cheung <cheungf@gmail.com>
2011-04-10 22:40:02 -0400

    add logo and signup page

--- index.rb
+++ index.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 require 'rack-flash'
+require 'digest/sha1'
 require 'models'
 require 'SMS'
 
@@ -9,11 +10,10 @@ use Rack::Flash
 enable :sessions
 
 helpers do
-
   def protected!
     unless authorized?
       response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
-      throw(:halt, [401, "Not authorized\n"])
+      throw(:halt, [401, "Not authorized\n PW=#{ENV['PS_PASSWORD']}"])
     end
   end
 
@@ -21,11 +21,21 @@ helpers do
     @auth ||=  Rack::Auth::Basic::Request.new(request.env)
     @auth.provided? && @auth.basic? && @auth.credentials == [ENV['PS_USER'], ENV['PS_PASSWORD']]
   end
-
 end
 
 get '/' do
   haml :index
+end
+
+get '/signup' do
+  haml :signup, :locals => {:action => "/signup"}
+end
+
+post '/signup' do
+  user = User.create(:password_hash => Digest::SHA1.hexdigest(params[:password]), 
+                     :username => params[:username], :phone => params[:phone]);
+  flash[:notice] = "Thanks for signing up! You will recieve your first mission within 72 hours."
+  redirect '/signup'
 end
 
 before '/admin/*' do
@@ -37,32 +47,33 @@ get '/admin/missions' do
 end
 
 get '/admin/missions/:id/edit' do |id|
-  protected!
-  haml :mission_update, :locals => {:mission => Mission.get(id), 
-                                    :action => "/admin/missions/#{id}/update"}
+  haml :mission_edit, :locals => {:mission => Mission.get(id), :action => "/admin/missions/#{id}/update"}
 end
 
 post '/admin/missions/:id/update' do |id|
-  protected!
   mission = Mission.get(id)
   mission.update params
   redirect '/admin/missions'
 end
 
 get '/admin/users' do
-  protected!
   haml :users_list, :locals => {:users => User.all}
 end
 
 get '/admin/users/:id/edit' do |id|
-  protected!
-  haml :user_update, :locals => {:user => User.get(id), :action => "/admin/users/#{id}/update"}
+  haml :user_edit, :locals => {:user => User.get(id), :action => "/admin/users/#{id}/update"}
 end
 
 post '/admin/users/:id/update' do |id|
-  protected!
   user = User.get(id)
   user.update params
+  redirect '/admin/users'
+end
+
+get '/admin/users/:id/delete' do |id|
+  user = User.get(id)
+  user.destroy
+  flash[:notice] = "Deleted User #{id}."
   redirect '/admin/users'
 end
 
